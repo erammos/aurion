55248484c3000f1d87728f73fdaef7da8b973e10
Elias Rammos <rammoselias@gmail.com>
2024-07-25 09:28:19 +0200

    testing obj loaders

--- src/test_ecs.c
+++ src/test_ecs.c
@@ -1,20 +1,12 @@
 #include <cglm/cglm.h>
-#include <flecs.h>
-#include "cglm/affine-pre.h"
-#include "cglm/call/io.h"
-#include "cglm/mat4.h"
-#include "flecs/addons/flecs_c.h"
 #include "minunit.h"
 #include <stdio.h>
-#include <stdlib.h>
+#define FAST_OBJ_IMPLEMENTATION
 
-typedef struct {
-    mat4 matrix;
-} Transform;
+#include "fast_obj.h"
 
-ECS_COMPONENT_DECLARE(Transform);
-ECS_TAG_DECLARE(Local);
-ECS_TAG_DECLARE(World);
+
+
 
 void
 test_setup(void) {}
@@ -23,70 +15,55 @@ void
 test_teardown(void) {
     /* Nothing */
 }
-
 MU_TEST(test_check) {
+ fastObjMesh* mesh = fast_obj_read("assets/test.obj");
 
-    ecs_world_t* ecs = ecs_init();
-    ECS_COMPONENT_DEFINE(ecs, Transform);
-	ECS_TAG(ecs,World);
-	ECS_TAG(ecs,Local);
-	ecs_entity_t hero = ecs_entity(ecs, { .name = "Hero" });
-	ecs_entity_t weapon = ecs_entity(ecs, { .name = "Weapon" });
+int idx = 0;
+for(int gi = 0 ; gi < mesh->group_count; gi++)
+{
+ fastObjGroup * grp = &mesh->groups[gi];
+ for(int i = 0 ; i < grp->face_count ; i++)
+ {
+    int fv = mesh->face_vertices[grp->face_offset + i];
 
-	ecs_set_pair(ecs,hero,Transform, World, {GLM_MAT4_IDENTITY_INIT});
-	ecs_set_pair(ecs,hero,Transform, Local, {GLM_MAT4_IDENTITY_INIT});
+    for(int j = 0 ; j < fv ; j++)
+    {
     
-	ecs_set_pair(ecs,weapon,Transform, World, {GLM_MAT4_IDENTITY_INIT});
-	ecs_set_pair(ecs,weapon,Transform, Local, {GLM_MAT4_IDENTITY_INIT});
-
-    ecs_add_pair(ecs,weapon, EcsChildOf,hero);
-
-      auto p = ecs_get_mut_pair(ecs, hero, Transform, Local);
-      glm_translate(p->matrix, (vec3) {0,10,0});
-
-     ecs_query_t *q = ecs_query(ecs, {
-        .terms = {
-            // Read from entity's Local position
-            { .id = ecs_pair(ecs_id(Transform), Local), .inout = EcsIn }, 
-            // Write to entity's World position
-            { .id = ecs_pair(ecs_id(Transform), World), .inout = EcsOut },
-
-            // Read from parent's World position
-            {
-                .id = ecs_pair(ecs_id(Transform), World), 
-                .inout = EcsIn,
-                // Get from the parent in breadth-first order (cascade)
-                .src.id = EcsCascade,
-                // Make parent term optional so we also match the root (sun)
-                .oper = EcsOptional
-            }
-        }
-    });
-
-    ecs_iter_t it = ecs_query_iter(ecs, q);
-    while (ecs_query_next(&it)) {
-        Transform *p = ecs_field(&it, Transform, 0);
-        Transform *p_out = ecs_field(&it, Transform, 1);
-        Transform *p_parent = ecs_field(&it, Transform, 2);
-        
-        // Inner loop, iterates entities in archetype
-        for (int i = 0; i < it.count; i ++) {
-            glm_mat4_copy(p->matrix, p_out->matrix);
-            if (p_parent) {
-                glm_mat4_mul(p_parent->matrix, p_out->matrix, p_out->matrix);
-            }
-        }
+        fastObjIndex mi = mesh->indices[grp->index_offset + idx];
+         if(mi.p)
+         {
+            float x = mesh->positions[3 * mi.p + 0];
+            float y = mesh->positions[3 * mi.p + 1];
+            float z = mesh->positions[3 * mi.p + 2];
+            printf ("pos: %f %f %f\n",x,y,z);
+         }
+          if (mi.t)
+                {
+                float u =  mesh->texcoords[2 * mi.t + 0];
+                float v = mesh->texcoords[2 * mi.t + 1];
+                printf ("uv: %f %f\n",u,v);
+                }
+           if(mi.n)
+           {
+              float nx = mesh->normals[3 * mi.n + 0];
+              float ny = mesh->normals[3 * mi.n + 1];
+              float nz = mesh->normals[3 * mi.n + 2];
+            printf ("normal: %f %f %f\n",nx,ny,nz);
+           }
+         idx++;
     }
-   it = ecs_each_pair_t(ecs, Transform, World);
-    while (ecs_each_next(&it)) {
-        Transform *p = ecs_field(&it, Transform, 0);
-        for (int i = 0; i < it.count; i ++) {
-            printf("%s: \n", ecs_get_name(ecs, it.entities[i]));
-            glmc_mat4_print(p[i].matrix, stdout);
-        }
+ }
+}
+printf("indices:\n");
+    for(int ii = 0 ; ii < mesh->index_count; ii++)
+    {
+       auto p = mesh->indices[ii];
+    
+       printf("%d \n", p.p);
     }
+ fast_obj_destroy(mesh);
 
-    ecs_fini(ecs);
+   
 }
 
 MU_TEST_SUITE(test_suite) {
